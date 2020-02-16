8271104fde358f8ca8282e916e132dee9191abc0
proxy707 <princegaur1998@gmail.com>
2020-02-16 12:28:44 +0530

    file added

--- BASIC/choosepattern.py
+++ BASIC/choosepattern.py
@@ -3,11 +3,11 @@ while(t):
     n=int(input("Enter no of rows: "))
     b=int(input("Enter choice: "))
     if(bool(b)==True):
-        
+
         for i in range(n):
             print("*" * (i+1), end ="")
             print("\n")
-    else:
+    else :
         for i in range(n):
             print("*"*(n-i), end="")
             print("\n")
