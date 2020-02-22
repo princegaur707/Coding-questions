8184d4fe659af3a641b93661fab18cd3160cfb32
proxy707 <princegaur1998@gmail.com>
2020-02-22 22:01:33 +0530

    file added

--- BASIC/Lambda/lambda2.py
+++ BASIC/Lambda/lambda2.py
@@ -2,5 +2,5 @@
 def a_first(a):
     return(a[1])
 a=[[6,4],[2,3],[1,8]]
-a.sort(key=a_first)
+a.sort(key=a_first)#key takes function which should return the 1st element
 print(a)
--- BASIC/fileIO/healthmana.py
+++ BASIC/fileIO/healthmana.py
@@ -1,17 +1,34 @@
 def getdate():
     import datetime
     return datetime.datetime.now()
+ch=int(input("Enter your choice\n\t\t(0-add 1-view): "))
 a=input("Enter your name: ")
-print(getdate())  
-if(a=="prince"):
-    with open ("prince.txt") as pri:
-        content=pri.readlines()
-        print("\n".join(content))
-elif(a=="abhishek"):
-    with open ("abhi.txt") as ab:
-        content=ab.readlines()
-        print("\n".join(content))
-elif(a=="ishjot"):
-    with open ("ish.txt") as isa:
-        content=isa.readlines()
-        print("\n".join(content))
+print(getdate()) 
+if(ch==0):
+    if(a=="prince"):
+        with open ("prince.txt",'a') as pri:
+            value=input("Type here: ")
+            content=pri.write(str(getdate())+":"+value+"\n")
+    elif(a=="abhishek"):
+        with open ("abhi.txt") as ab:
+            value=input("Type here: ")
+            content=ab.write(str(getdate())+":"+value+"\n")
+    elif(a=="ishjot"):
+        with open ("ish.txt") as isa:
+            value=input("Type here: ")
+            content=isa.write(str(getdate())+":"+value+"\n")
+
+else:
+    print(getdate())  
+    if(a=="prince"):
+        with open ("prince.txt") as pri:
+            content=pri.readlines()
+            print("\n".join(content))
+    elif(a=="abhishek"):
+        with open ("abhi.txt") as ab:
+            content=ab.readlines()
+            print("\n".join(content))
+    elif(a=="ishjot"):
+        with open ("ish.txt") as isa:
+            content=isa.readlines()
+            print("\n".join(content))
