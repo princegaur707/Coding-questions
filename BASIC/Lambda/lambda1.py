81ca8489441a02727f29e91ef829744fbfc55145
proxy707 <princegaur1998@gmail.com>
2020-02-22 10:34:54 +0530

    files changed

---
+++ BASIC/Lambda/lambda1.py
@@ -0,0 +1,7 @@
+#lambda functions or anonymous functions
+#It is a shortand technique to make a function
+#for "-"
+minus=lambda x,y:x-y
+x=int(input("Enter 1st number: "))
+y=int(input("Enter 2nd number: "))
+print(minus(x,y))
