0018d36b6453bf2200c91c2572c331a31e5aa5ec
proxy707 <princegaur1998@gmail.com>
2020-02-23 09:32:16 +0530

    files changed

---
+++ BASIC/games/swg.py
@@ -0,0 +1,38 @@
+import random
+i=1
+while(i>0):
+    score=0
+    score1=0
+    a=random.choice(['S','W','G'])
+    a1=input("Select your choice:\n\t\t[S-Snake  W-Water G-Gun: \t")
+    if(a=='S' and a=='W'):
+        score+=1
+    elif(a=='S' and a1=='G'):
+        print(f"I brought {a}")
+        score1+=1
+    elif(a=='W' and a1=='S'):
+        print(f"I brought {a}")
+        score1+=1
+    elif(a=='W' and a1=='G'):
+        print(f"I brought {a}")
+        score+=1
+    elif(a=='S' and a1=="W"):
+        print(f"I brought {a}")
+        score+=1
+    elif(a=='S' and a1=="G"):
+        print(f"I brought {a}")
+        score1+=1
+    else:
+        print(f"I brought {a}")
+        score+=1
+        score+=1
+    choice=input("Press 1 to continue")
+    if(choice=='1'):
+        i+=1
+    else:
+        break
+print(f"Your Score {score1}\n Our Score {score}\nRESULT:  ")
+if(score1>score):
+    print("Congratulations! You Win")
+else:
+    print("Sorry! You lost ")
---
+++ BASIC/practice.py
@@ -0,0 +1,4 @@
+def info(name, no_of_seats = 100, *marks):
+    pass
+info ("john", 350, 21)
+info ("john", 100, 21, 46)
