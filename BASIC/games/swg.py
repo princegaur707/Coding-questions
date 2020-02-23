da78b88fb69f98cc27535dedb847c8f622f1be27
proxy707 <princegaur1998@gmail.com>
2020-02-23 09:51:35 +0530

    files changed

--- BASIC/games/swg.py
+++ BASIC/games/swg.py
@@ -1,37 +1,37 @@
 import random
 i=1
 score=score1=0
-
 while(i>0):
-    a=random.choice(['S','W','G'])
-    a1=input("Select your choice:\n\t\t[S-Snake     W-Water     G-Gun]  : \t")
-    if(a=='S' and a=='W'):
+    a=random.choice(['s','w','g'])
+    a1=input("Select your choice:\n\t\t[s-Snake     w-Water     g-Gun]  : \t")
+    if(a=='s' and a=='w'):
+        print(f"I brought {a}")
         score+=1
-    elif(a=='S' and a1=='G'):
+    elif(a=='s' and a1=='g'):
         print(f"I brought {a}")
         score1+=1
-    elif(a=='W' and a1=='S'):
-        print(f"I brought {a}")
-        score+=1
-    elif(a=='W' and a1=='G'):
-        print(f"I brought {a}")
-        score+=1
-    elif(a=='G' and a1=="W"):
+    elif(a=='w' and a1=='s'):
         print(f"I brought {a}")
         score1+=1
-    elif(a=='G' and a1=="S"):
+    elif(a=='w' and a1=='g'):
+        print(f"I brought {a}")
+        score+=1
+    elif(a=='g' and a1=="w"):
+        print(f"I brought {a}")
+        score1+=1
+    elif(a=='g' and a1=="s"):
         print(f"I brought {a}")
         score+=1
     else:
         print(f"I brought {a}")
-        score+=1
+        score1+=1
         score+=1
     choice=input("Press 1 to continue: ")
     if(choice=='1'):
         i+=1
     else:
         break
-print(f"Your Score {score1}\n Our Score {score}\nRESULT:  ")
+print(f"Your Score {score1}\n Our Score {score}\nFINAL RESULT:  ")
 if(score1>score):
     print("Congratulations! You Win")
 elif(score1<score):
