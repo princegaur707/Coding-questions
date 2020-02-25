37362c72b1082e4157b7204e399420b6ca9b6507
proxy707 <princegaur1998@gmail.com>
2020-02-25 11:06:36 +0530

    file changed

--- BASIC/practice.py
+++ BASIC/practice.py
@@ -1,11 +1,17 @@
 
-def outer():
-  global glo
-  glo = 20
-  def inner():
-    global glo
-    glo = 30
-    print(glo)
-glo = 10
-outer()
-print( glo)
+# def outer():
+#   global glo
+#   glo = 20
+#   def inner():
+#     global glo
+#     glo = 30
+#     print(glo)
+# glo = 10
+# outer()
+# print( glo)
+
+dicts = dict()
+for l in enumerate(range(2)):
+    dicts[l[0]] = l[1]
+    dicts[l[1]+7] = l[0]
+print(dicts)
