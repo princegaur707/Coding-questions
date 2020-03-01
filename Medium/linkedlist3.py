d67bcd4838f9c62286f0a06b01375f43df10ecd6
proxy707 <princegaur1998@gmail.com>
2020-03-01 19:12:36 +0530

    files changed

---
+++ Medium/linkedlist3.py
@@ -0,0 +1,11 @@
+class node:
+    def __init__(self,data):
+        self.data=data
+        self.next=None 
+class LinkedList():
+    def __init(self):
+        self.start=None 
+    def insert(self,value):
+        newnode=node(value)
+        if(self.start==None):
+            self.start==newnode
