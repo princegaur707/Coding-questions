861f7d2658891759c86aac43710ea694a35dbb6a
proxy707 <princegaur1998@gmail.com>
2020-03-01 09:44:11 +0530

    files changed

--- BASIC/practice.py
+++ BASIC/practice.py
@@ -1,17 +1,41 @@
-
-# def outer():
-#   global glo
-#   glo = 20
-#   def inner():
-#     global glo
-#     glo = 30
-#     print(glo)
 class node:
     def __init__(self,data):
         self.data=data
         self.next=None
-        S
-    
+class LinkedList:
+    def __init__(self):
+        self.start=None
+    def insert(self,value):
+        newnode=node(value)
+        if(self.start==None):
+            self.start=newnode
+        else:
+            temp=self.start
+            while(temp.next!=None):
+                temp=temp.next
+            temp.next=newnode
+    def view(self):
+        if(self.start==None):
+            print("linked list is empty")
+        else:
+            temp=self.start
+            while(temp!=None):
+                print(temp.data,end=" ")
+                temp=temp.next
+    def deletefirst(self):
+            if(self.start==None):
+                print("Linied list is empty")
+            else:
+                self.start=self.start.next
+nlist=LinkedList()
+nlist.insert(10)
+nlist.insert(20)
+nlist.insert(30)
+nlist.view()
+print("\n")
+nlist.deletefirst()
+nlist.view()
+
 # glo = 10
 # outer()
 # print( glo)
@@ -25,7 +49,7 @@ class node:
 #     print (l[0],l[1])
 # print(dicts)
 # dig = 0
-# for i in range(0.0, 5.0, 0.1):
+# for i in range(0.0, 5.0, 0.1)
 #     dig += i
 #     i+=0
 # print(dig)
