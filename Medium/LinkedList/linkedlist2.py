26eea2b6e50a2b9b6acd7e09c94b6bd05af55235
proxy707 <princegaur1998@gmail.com>
2020-03-01 09:57:59 +0530

    files changed

--- Medium/LinkedList/linkedlist2.py
+++ Medium/LinkedList/linkedlist2.py
@@ -6,12 +6,12 @@ class node:
 class LinkedList:
     def __init__(self):
         self.start=None;
-    def deleteFirst(self):
+    def deletefirst(self):
         if self.start==None:
             print("Linked list is empty")
         else:
             self.start=self.start.next#no need to use anything to delete as python automatically deletes anything which is not pointed anywhere
-    def insertLast(self,value):
+    def insertlast(self,value):
         newNode= node(value)
         if(self.start==None):
             self.start=newNode   
@@ -19,3 +19,21 @@ class LinkedList:
             temp=self.start
             while temp.next!=None:
                 temp=temp.next
+            temp.next=newNode
+    def view(self):
+        if(self.start==None):
+            print("list is empty")
+        else:
+            temp=self.start
+            while temp!=None:
+                print(temp.data,end=" ")
+                temp=temp.next
+nlist=LinkedList()
+nlist.insertlast(1)
+nlist.insertlast(2)
+nlist.insertlast(3)
+nlist.insertlast(4)
+nlist.view()
+print("\n")
+nlist.deletefirst()
+nlist.view()
