589a67c54fb173bfe25350c8bd48d9616eb9dad8
proxy707 <princegaur1998@gmail.com>
2020-03-07 20:44:49 +0530

    files changed

--- BASIC/practice.py
+++ BASIC/practice.py
@@ -1,43 +1,52 @@
-import re
-temp='indiparker@ind.com'
-temp1=''
-if(re.search(r'@ind\.',temp)):
-    temp1=re.sub(r'i\w+(\.com)',r'edu\1',temp)
-print(temp1)
-# numbers=[0,1,4,8,6]
-# digit=numbers
-# digit[2:]=['91561','gerg']
-# print(numbers)
-# # print(int(not(7<7.0)))
-# # a_list=[1,9,8,4]
-# # print([item**2 for item in a_list])
-# a=b=[1]
-# c=d=[1],[1]
-# print(a is b)
-# print(c is d)
-# print("________________________")
-# for i in range (10):
-#     letters={'e','l','e','g','h','l','v'}
-#     print(f"set{letters}")
-#     new=list(letters)[::-1]
-# # print("new line new")
-#     print(new)
-# out=new[-2]*4
-# print(out)
-# # glo = 10
-# outer()
-# print( glo)
-#{0:1,8:0}
-# dicts = dict()
-# for l in enumerate(range(2)):
-#     print (dicts)
-#     print ("\n")
-#     dicts[l[0]] = l[1]
-#     dicts[l[1]+7] = l[0]
-#     print (l[0],l[1])
-# print(dicts)
-# dig = 0
-# for i in range(0.0, 5.0, 0.1)
-#     dig += i
-#     i+=0
-# print(dig)
+values=[1,2,1,3]
+nums=set(values)
+def checkit(num):
+    if num in nums:
+        return True
+    else:
+        return False
+for i in filter(checkit,values):
+    print(i)
+# import re
+# temp='indiparker@ind.com'
+# temp1=''
+# if(re.search(r'@ind\.',temp)):
+#     temp1=re.sub(r'i\w+(\.com)',r'edu\1',temp)
+# print(temp1)
+# # numbers=[0,1,4,8,6]
+# # digit=numbers
+# # digit[2:]=['91561','gerg']
+# # print(numbers)
+# # # print(int(not(7<7.0)))
+# # # a_list=[1,9,8,4]
+# # # print([item**2 for item in a_list])
+# # a=b=[1]
+# # c=d=[1],[1]
+# # print(a is b)
+# # print(c is d)
+# # print("________________________")
+# # for i in range (10):
+# #     letters={'e','l','e','g','h','l','v'}
+# #     print(f"set{letters}")
+# #     new=list(letters)[::-1]
+# # # print("new line new")
+# #     print(new)
+# # out=new[-2]*4
+# # print(out)
+# # # glo = 10
+# # outer()
+# # print( glo)
+# #{0:1,8:0}
+# # dicts = dict()
+# # for l in enumerate(range(2)):
+# #     print (dicts)
+# #     print ("\n")
+# #     dicts[l[0]] = l[1]
+# #     dicts[l[1]+7] = l[0]
+# #     print (l[0],l[1])
+# # print(dicts)
+# # dig = 0
+# # for i in range(0.0, 5.0, 0.1)
+# #     dig += i
+# #     i+=0
+# # print(dig)
