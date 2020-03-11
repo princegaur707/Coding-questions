2be032b3cd7ee0cb3167bf77c1500ee33d68a7c9
proxy707 <princegaur1998@gmail.com>
2020-03-11 21:08:08 +0530

    files changed

---
+++ BASIC/OOPS/oops13.py
@@ -0,0 +1,4 @@
+#Polymorphism: Ability to take various forms
+print(5+6)
+print("5"+"6")
+# here when we apply it on integers we get different answer to when it is applied to string this conveys us the idea of polymorphism
