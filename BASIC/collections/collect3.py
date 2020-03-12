59a393730f4164b8c38a0535088ec0ff0469b25e
proxy707 <princegaur1998@gmail.com>
2020-03-12 11:19:08 +0530

    files changed

--- BASIC/collections/collect3.py
+++ BASIC/collections/collect3.py
@@ -6,13 +6,16 @@ d.append(1)
 d.append(2)
 d.append(3)
 
-d.extend('abc') #This will append a,b,c while append would have considered abc as one element
-d.append('abc')
+d.extend('456') #This will append a,b,c while append would have considered abc as one element
+d.append('456')
 print(d)
-d.extend([4,5,6]) #extend will even go inside the list and print iterables
-d.append([4,5,6])
+d.extend([7,8,9,10]) #extend will even go inside the list and print iterables
+d.append([7,8,9])
 print(d)
-d.remove(1)
+d.remove(10) #remove only once and 1st occurence only
 print(d)
 print (d.pop())
 print (d.popleft())
+print(d)
+d.rotate(-3) #rotate by 3 to the left
+print(d)
