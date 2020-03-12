4381015ddd2de1c81023c7f1e6e10974fb6903e4
proxy707 <princegaur1998@gmail.com>
2020-03-12 21:57:24 +0530

    files changed

--- BASIC/collections/collect4.py
+++ BASIC/collections/collect4.py
@@ -1,17 +1,23 @@
 #Counter
 from collections import Counter
 
-c=Counter("Allahabad")
-print(c) #elements are arranged in decreasing order of frequency
 print("________________________________________________________________________________________")
+c=Counter("Allahabad")
+print(c) #elements are arranged in decreasing order of frequency in form of dictionary with frequency as a value
+print("________________________________________________________________________________________")
+
 for i in c.elements():
     print(i,end=" ")
 print("\n________________________________________________________________________________________")
-print(c.elements())#-> It will print garbage value when directly printed because it returns an itertool, not a specific data-container.
+
+print(c.elements())#It prints a location because it returns an itertool, not a specific data-container.
 print("________________________________________________________________________________________")
-x = Counter (a = 2, x = 3, b = 3, z = 1, y = 5, c = 0, d = -3) 
+
 print(list(c.elements()))
 print("________________________________________________________________________________________")
+
+x = Counter (a = 2, x = 3, b = 3, z = 1, y = 5, c = 0, d = -3) 
 for i in x.elements(): 
     print( "% s : % s" % (i, x[i]), end ="||") 
-#If the count of an item is already initialized in Counter object, then it will ignore the ones with zero and negative values.
+print("\n________________________________________________________________________________________")
+#If the count of an item is already initialized in Counter object, then it will ignore the ones with zero and negative values
