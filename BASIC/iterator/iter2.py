217b5927f97150322de998d0d6775e546c9c8848
proxy707 <princegaur1998@gmail.com>
2020-03-14 18:15:05 +0530

    files changed

--- BASIC/iterator/iter2.py
+++ BASIC/iterator/iter2.py
@@ -1,9 +1,16 @@
 #combinations(iterable,r)
-from itertools import combinations
+from itertools import combinations,combinations_with_replacement
 
 print(list(combinations(range(3),2)))
-#OUT:
+#OUT:[(0, 1), (0, 2), (1, 2)]  
 #this is different from permutation as that would have also given [(0,1)(1,0)]
 print(list(combinations('abc',2)))
 
 print(list(combinations('abc',3)))
+
+print(list(combinations_with_replacement(range(3),2)))
+#Repetition is allowed 
+
+print(list(combinations_with_replacement('abc',2)))
+
+print(list(combinations_with_replacement('abc',2)))
