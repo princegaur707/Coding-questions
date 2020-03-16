2e59a0e27f7ba78c02a43ccd33cb16ba5b43c263
proxy707 <princegaur1998@gmail.com>
2020-03-16 21:13:45 +0530

    files changed

---
+++ BASIC/fileIO/healthman2.py
@@ -0,0 +1,15 @@
+from pygame import mixer 
+
+def musicloop(file,stopper):#file having music stopper: when we wanna stop
+    mixer.init()
+    mixer.music.load(file)
+    mixer.music.play()
+    while True:
+        a=input()
+        if a==stopper:
+            mixer.music.stop()
+            break
+
+if __name__=="__main__":
+    musiconloop=("water.mp3","stop")
+
