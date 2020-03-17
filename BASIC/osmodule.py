0a5d01350e5c5787cd5a1c8f2b1272d84b2fb7e6
proxy707 <princegaur1998@gmail.com>
2020-03-17 11:30:51 +0530

    files changed

---
+++ BASIC/osmodule.py
@@ -0,0 +1,13 @@
+import os
+
+#print(dir(os))
+#print(os.getcwd()) #get current working directory
+#os.chdir(("C://")) #we can current working directory using change directory
+#print(os.listdir("P://")) #gives list of all directory present at the current location
+#os.mkdir("This")#to make directory
+#os.mkdirs("A/B") #to make directories('B' will be inside of 'A')
+#os.rename("osmodule.py","osmodule1.py") #to rename a directory
+#print(os.environ.get('Path'))
+#print(os.path.join("C://","prince.txt")) #to add two paths and it will automatically correct the slashes
+#print(os.path.exists("C://programfiles2")) #to check existence of any file at any specific path
+#print(os.path.isfile("C://programfiles2")) #any directory will not be considered file(return bool value)
