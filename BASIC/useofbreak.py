874621bbfec38b188a64e21f8e02148385478d02
proxy707 <princegaur1998@gmail.com>
2020-03-18 10:27:44 +0530

    files changed

--- BASIC/useofbreak.py
+++ BASIC/useofbreak.py
@@ -1,14 +1,17 @@
 while(True):
-    n=int(input())
-    if(n<100):
-        print("if")
-        break
-    else:
-        print("Number must be less than 100")
-        break
-        print("else")
-    print("while")
-print("No")
+    print("Prince")
+    while(True):
+        n=int(input())
+        #break #it moves the pointer outside the body of loop
+        if(n<100):
+            print("if")
+            #break
+        else:
+            print("Number must be less than 100")
+            print("else")
+        print("while")
+    print("No") #this is not inside the body of while loop
+print("gaur")
 # while(True):
 #     n=int(input())
 #     if(n<100):
