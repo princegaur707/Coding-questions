c2e254219c93a6720d1ce8cf3dfcd550f24a88e6
proxy707 <princegaur1998@gmail.com>
2020-03-24 23:18:04 +0530

    file changed

--- BASIC/Lambda/lambda2.py
+++ BASIC/Lambda/lambda2.py
@@ -1,6 +1,9 @@
 #lambda function for sorting
 def a_first(a):
-    return(a[1])
+    return(a[1]) #it is saying to sort according to 1st argument
 a=[[6,4],[2,3],[1,8]]
 a.sort(key=a_first)#key takes function which should return the 1st element
+print(a)
+
+a.sort(key=lambda i:i[0])#It is saying to sort according to 0th argument
 print(a)
