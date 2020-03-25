48cb104156b00bd5e4d1ee9d5bf011419a320b17
proxy707 <princegaur1998@gmail.com>
2020-03-25 10:48:59 +0530

    files changed

---
+++ BASIC/iterator/iter4.py
@@ -0,0 +1,8 @@
+# takewhile - takes items from an iterable while a predicate function remains true; exactly like filter
+# chain - combines several iterables into one long one;
+# accumulate - returns a running total of values in an iterable(sum total of iterables currently working on)
+
+from itertools import takewhile
+nums = [2, 4, 6, 7, 9, 8]
+a = takewhile(lambda x: x%2==0, nums)
+print(list(a))
