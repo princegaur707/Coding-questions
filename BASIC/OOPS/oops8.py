b07b506c546614492de5f6eca449014fd6e37f9a
proxy707 <princegaur1998@gmail.com>
2020-03-25 13:54:44 +0530

    files changed

--- BASIC/OOPS/oops8.py
+++ BASIC/OOPS/oops8.py
@@ -7,11 +7,11 @@ class Employee:
         self.salary=asalary
     def printdetails(self):
         return f"name is {self.name} salary is {self.salary}"
-
    
     @staticmethod
     def printsome(string):
         print(f"You are good {string}")
+
 harry= Employee("Harry",555) 
 larry=Employee("Larry",556)
 karan=Employee.printsome("Karan")
