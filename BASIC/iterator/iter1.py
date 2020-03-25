62ee7f3cfb048c891344dbce39f0c783baa255dd
proxy707 <princegaur1998@gmail.com>
2020-03-25 10:32:06 +0530

    files changed

--- BASIC/iterator/iter1.py
+++ BASIC/iterator/iter1.py
@@ -1,5 +1,6 @@
 from itertools import product
 from itertools import permutations
+from itertools import repeat
 
 print(list(product([1,3,5],[2,5,6],[1,3,5])))
 
@@ -10,3 +11,15 @@ print(list(permutations([1,2,3],2)))# here 2 implies 2 elements at a time
 print(list(permutations(range(3))))
 
 print(list(permutations('abc',2)))#[('a', 'b'), ('a', 'c'), ('b', 'a'), ('b', 'c'), ('c', 'a'), ('c', 'b')]
+
+
+# The function count counts up infinitely from a value.
+#count has two arguments: count(x, y) where x is a start value and y is the increment/decrement pace (with y being a positive or negative number, respectively).
+# The function cycle infinitely iterates through an iterable (for instance a list or string).
+# The function repeat repeats an object, either infinitely or a specific number of times
+
+for i in cycle("Huri"):
+    print(i)
+
+# Gimme a list which has "Huri" string 5 times
+print(list(repeat("Huri", 5))
--- BASIC/recursion/factorial.py
+++ BASIC/recursion/factorial.py
@@ -1,6 +1,6 @@
 #Recursive approach
 def factorial(n):
-    if(n<=1):
+    if(n==1):
         return(1)
     else:
         return(n*factorial(n-1))
