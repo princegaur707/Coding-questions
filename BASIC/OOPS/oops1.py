2fb343e198e105e402423047c7dc7714b2b08af0
proxy707 <princegaur1998@gmail.com>
2020-03-25 11:30:03 +0530

    files changed

--- BASIC/OOPS/oops1.py
+++ BASIC/OOPS/oops1.py
@@ -3,6 +3,16 @@
 #Why to make Class?
 #Python follow DRY: Don't repeat yourself
 #To restrict the access of some functions or objects
+# #self refers to the instance calling the method.
+# you should know that it is not necessary to write self as a word.
+# koky=Cat('white', 4)
+# now in the class itself 
+# class Cat:
+#          def__init__( myobject, color, legs) :
+#                   myobject.catcolor = color 
+#                   myobject.catlegs = legs
+
+# here koky is just referring to the object of the class  by using myobject
 class Student: #Try to name class with capital letter
     pass    #to not to get the error
 
