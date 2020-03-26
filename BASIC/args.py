33e290d86cf8e16f8670b5e0e423650948ad2e2a
proxy707 <princegaur1998@gmail.com>
2020-03-26 21:27:41 +0530

    files changed

--- BASIC/args.py
+++ BASIC/args.py
@@ -4,10 +4,10 @@ name="prince"
 name1=['prince','gaur','is','doing', 'work']
 name2={'Name':'Prince','sirname':'gaur','action':'work'}
 func(name,*name1,**name2)
-#Using *args as a function parameter enables you to pass an arbitrary number of arguments to that function.
+#Using *args as a function parameter enables  to pass an arbitrary number of arguments to that function.
 #The arguments are then accessible as the tuple args in the body of the function.
 
-#**kwargs (standing for keyword arguments) allows you to handle named arguments that you have not defined in advance.
+#**kwargs (standing for keyword arguments) allows  to handle named arguments {that we have not defined in advance}.
 #The keyword arguments return a dictionary in which the keys are the argument names, and the values are the argument values.
 
 #Named parameters to a function can be made optional by giving them a default value.
