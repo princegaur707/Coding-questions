541ca30ab0bb9cb85c40a8a3220cc26b4c297a55
proxy707 <princegaur1998@gmail.com>
2020-03-27 11:58:20 +0530

    files changed

--- BASIC/regex/regex4.py
+++ BASIC/regex/regex4.py
@@ -7,7 +7,7 @@ newstr = re.sub(pattern, "Amy", str)
 print(newstr)
 # Character classes provide a way to match only one of a specific set of characters.
 # A character class is created by putting the characters it matches inside square brackets.
-pattern = r"[aeiou]"
+pattern = r"[aeiou]" #matches any of the character
 if re.search(pattern, "grey"):
    print("Match 1")
 if re.search(pattern, "qwertyuiop"):
