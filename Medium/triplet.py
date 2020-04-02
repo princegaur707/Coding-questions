112746759a49ea0f45f1b86597b8b838ad2cc45d
proxy707 <princegaur1998@gmail.com>
2020-04-02 23:17:37 +0530

    file changed

--- Medium/triplet.py
+++ Medium/triplet.py
@@ -3,22 +3,18 @@ while (t>0):
     n=int(input())
     a=list(map(int,input().split()))[:n]
     s=0
-    big=[]
-    sma=[]
-    for i in range(n):
-        s=0
-        for j in range(n):
-            if(a[i]>a[j]):
-                s+=1
-        sma.append(s)
-        s=0
-        for j in range(n):
-            if(a[i]<a[j]):
-                s+=1
-        big.append(s)
-    print(sma)
-    print(big)
-    for i in range(n):
-        s+=big[i]*sma[i]
+    for i in range(0,len(a),1):
+        ss=sg=0
+        j=i-1
+        while(j>=0): 
+            if(a[i]>a[j]): 
+                ss+=1
+            j-=1
+        j=i+1
+        while(j<n):
+            if(a[i]<a[j]): 
+                sg+=1
+            j+=1
+        s+=ss*sg
     print(s)
     t-=1
