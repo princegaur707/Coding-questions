ac941c21da552414622477d6f9b08f32e0e3211a
proxy707 <princegaur1998@gmail.com>
2020-04-05 14:48:33 +0530

    file added

--- Medium/strangenum.py
+++ Medium/strangenum.py
@@ -1,36 +1,51 @@
 import math
 from math import ceil,sqrt
+arr=[]
 t=int(input())
 while t:
     x,k=map(int,input().split())
-    def isprime(n):
+    def chkprime(n):
         if n<=1:
-            return 0
-        if n<=3:
-            return 1
-        if n%2==0 or n%3==0 :
-            return 0
-        for i in range(5,ceil(sqrt(n))+1,6):
+            return False
+        if(n<=3):
+            return True
+        if(n%2==0 or n%3==0):
+            return False
+        for i in range(1,ceil(sqrt(n))+1,6):
             if n%i==0 or n%(i+2)==0:
-                return 0
-        return 1
+                return False
+        return True
     def numdivisors(n):
         cnt=0
-        for i in range(1,n+1):
+        arr.clear()
+        for i in range(1,int(math.sqrt(n)+1)):
             if(n%i==0):
-                cnt+=1
-        return cnt
-    def divprimecount(n):
-        cnt=0
-        for i in range(1,n+1):
-            if n%i==0:
-                if isprime(i):
+                if n/i==i:
                     cnt+=1
+                    arr.append(i)
+                else:
+                    cnt+=2
+                    arr.append(i)
+                    arr.append(n/i)
+        #print("Number of divisors:",cnt)
+        #print("divisors array",arr)
         return cnt
-    for i in range(200):
+    def primecount():
+        cnt1=0
+        for i in range(len(arr)):
+            if(chkprime(arr[i])):
+                cnt1+=1
+        #print("Number of primes:",cnt1)
+        return cnt1
+    c=0
+    for i in range(1,100):
+        #print("\ncurrent number:  ",i)
         if(numdivisors(i)==x):
-            if(divprimecount(i)==k):
-                print(i)
+            if(primecount()==k):
+                c=1
+                print(1)
                 break
+    if(c==0):
+        print(0)
     t-=1
 
