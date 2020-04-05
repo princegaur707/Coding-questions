ba080987c9aef99368324b0d143e1596ed8973cb
proxy707 <princegaur1998@gmail.com>
2020-04-05 10:10:23 +0530

    file changed

--- Medium/strangenum.py
+++ Medium/strangenum.py
@@ -1,33 +1,36 @@
+import math
+from math import ceil,sqrt
 t=int(input())
 while t:
     x,k=map(int,input().split())
-    arr=[]
-    c=0
     def isprime(n):
-        if(n<=1):
-            return False
-        if(n<=3):
-            return True
-        if(n%2==0 or n%3==0 or n%5==0):
-            return False
-        for i in range(5,ceil(sqrt(n)),6):
-            if(n%i==0 or n%(i+2)==0):
-                return False
-        return True
-        
-    for i in range x**10*k:
-        for j in range i:
-            if(i%j==0):
+        if n<=1:
+            return 0
+        if n<=3:
+            return 1
+        if n%2==0 or n%3==0 :
+            return 0
+        for i in range(5,ceil(sqrt(n))+1,6):
+            if n%i==0 or n%(i+2)==0:
+                return 0
+        return 1
+    def numdivisors(n):
+        cnt=0
+        for i in range(1,n+1):
+            if(n%i==0):
                 cnt+=1
-                arr.append(j)
-                if(cnt>x):
-                    break
-    if(cnt==x):
-        for i  in range(len(arr)):
-            if(isprime(arr[i])):
-                cnt1+=1
-        if(cnt1==k):
-            print(1)
-            c=1
-    if(c==0):
-        print(0)
+        return cnt
+    def divprimecount(n):
+        cnt=0
+        for i in range(1,n+1):
+            if n%i==0:
+                if isprime(i):
+                    cnt+=1
+        return cnt
+    for i in range(200):
+        if(numdivisors(i)==x):
+            if(divprimecount(i)==k):
+                print(i)
+                break
+    t-=1
+
