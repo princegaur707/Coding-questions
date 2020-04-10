4991ec38cd41d80c201badc2f9937a42109e4eef
proxy707 <princegaur1998@gmail.com>
2020-04-11 00:43:43 +0530

    file changed

--- BASIC/comprehensions.py
+++ BASIC/comprehensions.py
@@ -1,17 +1,20 @@
 #list comprehensions: using []
 ls=[i for i in range(100) if i%3==0]# in one line list of all numbers divisible by 3
 print(ls)
+print("1________________________________________________________________________________________________________________________________________")
 #comp.dictionary: using{key:value}
 #dict1={0:'item0',1:'item1',.............}
 dict1={ i:f"item {i}" for i in range(10) if i%2==0}
 print(dict1)
+print("2________________________________________________________________________________________________________________________________________")
 #for reversing the key and values in dictionary
 dict1={value:key for key,value in dict1.items()}
 print(dict1)
-
+print("3________________________________________________________________________________________________________________________________________")
 #comprehension set: using {}(this is like comprehension list but they return unique values)
 set1={i for i in [1,2,1,5,35,25,2,35]}
 print(set1)
+print("4________________________________________________________________________________________________________________________________________")
 #comp. generators: using ()
 gen1=(i for i in range(10) if i%2==0) 
 print(gen1)
--- BASIC/iterator/iter2.py
+++ BASIC/iterator/iter2.py
@@ -4,13 +4,13 @@ from itertools import combinations,combinations_with_replacement
 print(list(combinations(range(3),2)))
 #OUT:[(0, 1), (0, 2), (1, 2)]  range(3): 0,1,2 elements to be used ---- 2: with 2 elements in one 
 #this is different from permutation as that would have also given [(0,1)(1,0)]
+print("______________________________________________________________________________________________________________________________________________")
 print(list(combinations('abc',2)))
-
+print("______________________________________________________________________________________________________________________________________________")
 print(list(combinations('abc',3)))
-
-print(list(combinations_with_replacement(range(3),2)))
-#Repetition is allowed 
-
+print("______________________________________________________________________________________________________________________________________________")
+print(list(combinations_with_replacement(range(3),2)))#Repetition is allowed 
+print("______________________________________________________________________________________________________________________________________________")
 print(list(combinations_with_replacement('abc',2)))
-
+print("______________________________________________________________________________________________________________________________________________")
 print(list(combinations_with_replacement('abc',2))) # it will allow ('a','a') also
