9263cbefdb55d602199e92f93ce3d2909f331801
proxy707 <princegaur1998@gmail.com>
2020-04-12 23:23:58 +0530

    file changed

--- Medium/kclosest.py
+++ Medium/kclosest.py
@@ -37,7 +37,24 @@ class Solution:
             r+=1
             cnt+=1
         return reversearr
-                
+class Solution:
+    def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
+        if len(arr) == k:
+            return arr
+        
+        start, end = 0, len(arr)-k
+        while start+1 < end:
+            mid = start+(end-start)//2
+            if abs(x-arr[mid]) <= abs(arr[mid+k]-x): # start is more preferred
+                end = mid
+            else: # end is more preferred
+                start = mid
+        
+        if abs(x-arr[start]) <= abs(arr[start+k]-x):
+            return arr[start:start+k]
+       	else:
+            return arr[end:end+k]
+                   
             
             
         
