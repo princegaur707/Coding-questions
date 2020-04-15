d5bd7b64c6e04e32a163745864e1243e7f4e355e
proxy707 <princegaur1998@gmail.com>
2020-04-15 18:11:04 +0530

    files added

--- Medium/subpro.py
+++ Medium/subpro.py
@@ -10,3 +10,16 @@ class Solution:
         return max(A + B)
 obj=Solution()
 print(obj.maxProduct([1,2,-4,1]))
+
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        i = 0
+        currentMax, currentMin, ans = nums[0], nums[0], nums[0]
+        for i in range(1, len(nums)):
+            n = nums[i]
+            tmp = currentMax
+            currentMax = max(n, n*currentMax, n*currentMin)
+            currentMin = min(n, n*tmp, n*currentMin)
+            ans = max(ans, currentMax)
+        return ans
+            
