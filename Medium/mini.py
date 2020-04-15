fc0a20e9cde8800987ad002378246a1dfdad854d
proxy707 <princegaur1998@gmail.com>
2020-04-15 23:23:34 +0530

    files added

---
+++ Medium/mini.py
@@ -0,0 +1,14 @@
+#https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/
+#find minimum of array obtained after rotating from certain point
+class Solution:
+    def findMin(self, nums):
+        left, right = 0, len(nums)-1   
+        while left < right:
+            mid = (left + right) // 2           
+            if nums[mid] > nums[right]:
+                left = mid + 1
+            else:  
+                right = mid
+        return nums[left]
+obj=Solution()
+print(obj.findMin([3,4,5,6,1,2]))
--- Medium/subpro.py
+++ Medium/subpro.py
@@ -1,25 +1,17 @@
 #https://leetcode.com/problems/maximum-product-subarray/
-class Solution:
-    def maxProduct(self, A):
-        B = A[::-1]
-        for i in range(1, len(A)):
-            A[i] *= A[i - 1] or 1
-            B[i] *= B[i - 1] or 1
-        print(A)
-        print(B)
-        return max(A + B)
-obj=Solution()
-print(obj.maxProduct([1,2,-4,1]))
 
 class Solution:
-    def maxProduct(self, nums: List[int]) -> int:
-        i = 0
-        currentMax, currentMin, ans = nums[0], nums[0], nums[0]
+    def maxProduct(self, nums):
+        currentMax, currentMin, ans =ans1 = nums[0], nums[0], nums[0]
         for i in range(1, len(nums)):
             n = nums[i]
             tmp = currentMax
-            currentMax = max(n, n*currentMax, n*currentMin)
-            currentMin = min(n, n*tmp, n*currentMin)
+            currentMax = max(n, n*currentMax, n*currentMin) 
+            currentMin = min(n, n*tmp, n*currentMin) # It is required as in case of [-2,3,-4] at last iteration it will yiled 24 and others -4,-12 
             ans = max(ans, currentMax)
-        return ans
-            
+            ans1= min(ans,currentMin)
+            print(f"n: {n} tmp: {tmp} MAX:{currentMax} MIN: {currentMin} Ans:{ans} Ans1:{ans1}")
+        return [ans,ans1]
+obj=Solution()
+print(obj.maxProduct([-2,3,-4]))          
+ 
