c78f2fbb2873b038812115bd23a4fcb77a873a89
proxy707 <princegaur1998@gmail.com>
2020-04-16 22:21:03 +0530

    file changed

---
+++ outputs/prepostshift1.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<stdio.h>
+using namespace std;
+int main()
+{
+	int a;
+	a=1;
+	while(a<=1)
+	if(a%2)
+	printf("%d",a++);
+	else
+	printf("%d",++a);
+	printf("%d",a+10);
+}
