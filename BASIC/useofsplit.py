91a1eaf99c4daaa1abd14fe14af2ee28196d036f
proxy707 <princegaur1998@gmail.com>
2020-04-16 11:58:40 +0530

    files added

---
+++ BASIC/useofsplit.py
@@ -0,0 +1,14 @@
+i = '(a,a),(b,b),(c,c),(d,d)'
+
+l = []
+
+for tup in i.split('),('):   #considering all 3 i.e, ) , ( as parameters to split
+    #tup looks like `(a,a` or `b,b`
+    print(f"first     {tup}")
+    tup = tup.replace(')','').replace('(','')
+    print(f"second    {tup}")
+    #tup looks like `a,a` or `b,b`
+    l.append(tuple(tup.split(',')))
+    print(l)
+
+print(l)
