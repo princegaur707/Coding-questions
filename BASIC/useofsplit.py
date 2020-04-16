a3b410be740f79cebeb5d7350a60481b493ff21d
proxy707 <princegaur1998@gmail.com>
2020-04-16 21:11:02 +0530

    file changed

--- BASIC/useofsplit.py
+++ BASIC/useofsplit.py
@@ -1,12 +1,12 @@
-i = '(a,a),(b,b),(c,c),(d,d)'
+i = '(a,a),(b,b),(c,c),(d,d)'#converting this to [('a', 'a'), ('b', 'b'), ('c', 'c'), ('d', 'd')]
 
 l = []
 
 for tup in i.split('),('):   #considering all 3 i.e, ) , ( as parameters to split
     #tup looks like `(a,a` or `b,b`
-    print(f"first     {tup}")
+    #print(f"first     {tup}")
     tup = tup.replace(')','').replace('(','')
-    print(f"second    {tup}")
+    #print(f"second    {tup}")
     #tup looks like `a,a` or `b,b`
     l.append(tuple(tup.split(',')))
     print(l)
