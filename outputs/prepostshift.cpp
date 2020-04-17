5cdb5fc67792e7fe91afdf5a5ef6f8a4ea845763
proxy707 <princegaur1998@gmail.com>
2020-04-17 13:31:06 +0530

    file changed

--- outputs/prepostshift.cpp
+++ outputs/prepostshift.cpp
@@ -4,8 +4,8 @@ int array[10] = {3, 0, 8, 1, 12, 8, 9, 2, 13, 10};
 int x, y, z;
 x = ++array[2];
 y = array[2]++;
-z = array[x++];
-printf("%d %d %d", x, y, z);
+z = array[x++]; //x will be 10 as it is being increased here also
+printf("%d %d %d", x, y, z);  
 
 return 0;
 }
