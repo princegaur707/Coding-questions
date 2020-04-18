0f142ad4b68a920c978a3a2d4d972895a00762d9
proxy707 <princegaur1998@gmail.com>
2020-04-18 23:56:52 +0530

    file changed

--- Medium/practice.py
+++ Medium/practice.py
@@ -1,11 +1,14 @@
 #Find two numbers who sum is equal to target
 class Solution:
     def twoSum(self, nums, target):
-        dict1 = {}
+        dict1={}
+        for i in range(len(nums)):
+            dict1[i]=nums[i]
         dict1={value:key for key,value in dict1.items()}
-        for key,value in dict1:
+        print(dict1)
+        for key,value in dict1.items():
             try:
-                return [dict1[target - n], i]
+                return [dict1[target - key], value]
             except:
                 dict1[n] = i
 obj=Solution()
