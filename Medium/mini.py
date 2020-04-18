a7a791c5d60ae77fd403b1b2daed3ce076766c5e
proxy707 <princegaur1998@gmail.com>
2020-04-18 22:47:46 +0530

    file changed

--- Medium/mini.py
+++ Medium/mini.py
@@ -4,11 +4,13 @@ class Solution:
     def findMin(self, nums):
         left, right = 0, len(nums)-1   
         while left < right:
+            print(left,right)
             mid = (left + right) // 2           
             if nums[mid] > nums[right]:
                 left = mid + 1
             else:  
                 right = mid
+            print("MID:",mid)
         return nums[left]
 obj=Solution()
 print(obj.findMin([3,4,5,6,1,2]))
