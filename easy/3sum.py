a94899d104350c5c8f7fd7f1ea23864de77dfbd3
proxy707 <princegaur1998@gmail.com>
2020-04-21 09:35:59 +0530

    file changed

--- easy/3sum.py
+++ easy/3sum.py
@@ -1,18 +1,29 @@
+import bisect
 class Solution:
     def threeSum(self, nums):
-        if len(nums) < 3:
-            return []
-        nums.sort()
-        res = set()
-        for i, v in enumerate(nums[:-2]):
-            if i >= 1 and v == nums[i-1]:
-                continue
-            d = {}
-            for x in nums[i+1:]:
-                if x not in d:
-                    d[-v-x] = 1
-                else:
-                    res.add((v, -v-x, x))
-        return [*map(list,res)]
+        ref = {}
+        for i in nums:
+            if i not in ref:
+                ref[i] = 1
+            else:
+                ref[i] += 1
+        nums=sorted(ref)
+        ans=[]
+        for i, itemI in enumerate(nums):
+            if itemI == 0:
+                if ref[itemI] > 2:
+                    ans.append((0, 0, 0))
+            else:
+                if ref[itemI] > 1 and -2 * itemI in ref:
+                    ans.append((itemI, itemI, -2 * itemI))
+            if itemI < 0:
+                target = -itemI
+                left = bisect.bisect_left(nums, target-nums[-1], i+1)
+                right = bisect.bisect_right(nums, target // 2, left)
+                for itemJ in nums[left : right]:
+                    itemK = target - itemJ
+                    if itemK in ref and itemK != itemJ:
+                        ans.append((itemI, itemJ, itemK))
+        return ans
 obj=Solution()
 print(obj.threeSum([-1, 0, 1, 2, -1, -4]))
