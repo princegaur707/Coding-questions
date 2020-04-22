ebf1d9da248c238d0df38efc4e5eb5cdce335afb
proxy707 <princegaur1998@gmail.com>
2020-04-22 09:35:24 +0530

    file changed

--- easy/practice.py
+++ easy/practice.py
@@ -1,18 +1,6 @@
-class Solution:
-    def findClosestElements(self, arr, k, x):
-        left, right = 0, len(arr) - k
-        while left < right:
-            print("Left,Right:   ",left,right)
-            mid = (left+right)//2
-            print("Mid:   ",mid)
-            if x - arr[mid] > arr[mid + k] - x:
-                print(x-arr[mid])
-                print(arr[mid+k]-x)
-                print("IF")
-                left = mid + 1
-            else:
-                print("Else")
-                right = mid
-        return arr[left:left + k]
-obj=Solution()
-print(obj.findClosestElements([1,2,3,4,6,7,8,9,10],4,9))
+from collections import Counter
+def threesums(nums):
+    ref=Counter(nums)
+    print(ref)
+threesums([-1, 0, 1, 2, -1, -4])
+        
