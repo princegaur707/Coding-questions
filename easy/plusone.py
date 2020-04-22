42f036c41f96b810ee79a4dce2095a088c0f7956
proxy707 <princegaur1998@gmail.com>
2020-04-23 03:52:57 +0530

    file changed

--- easy/plusone.py
+++ easy/plusone.py
@@ -1,24 +1,24 @@
+#https://leetcode.com/explore/featured/card/top-interview-questions-easy/92/array/559/
 class Solution:
-    def plusOne(self, digits):
-        if digits[-1] != 9:
-            digits[-1] += 1
-            return digits
+    def plusOne(self, digits: List[int]) -> List[int]:
+        if digits[-1]!=9:
+            digits[-1]+=1
         else:
-            digits[-1] = 0
-            c = 1
-            for i in range(len(digits) - 2, -1, -1):
+            digits[-1]=0
+            c=1
+            for i in range(len(digits)-2,-1,-1):
                 if c:
-                    if digits[i] == 9:
-                        digits[i] = 0
-                        c = 1
+                    if digits[i]!=9:
+                        digits[i]+=1
+                        c=0
                     else:
-                        digits[i] += c
-                        c = 0
+                        digits[i]=0
             if c:
-                digits.insert(0, 1)
-            return digits
+                digits.insert(0,1)
+        return digits
 obj=Solution()
-print(obj.plusOne([1,2,3]))
+print(obj.plusOne([9,9,9]))
+
 
 # class Solution:
 #     def plusOne(self, digits):
