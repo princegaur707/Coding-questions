c8885e248bb633ffb3bd0ef1514a429f6e446e12
proxy707 <princegaur1998@gmail.com>
2020-04-25 12:51:37 +0530

    file added

--- easy/validpalindrome.py
+++ easy/validpalindrome.py
@@ -2,10 +2,12 @@ class Solution:
     def isPalindrome(self, s):
         string=""
         for i in s:
-            if i.isalpha():
-                string.join(i)
-                print(string)
-        string=string.replace(" ","")        
-        string=string.lower()
-        return s==s[::-1]
+            if i.isalnum():
+                string+=i.lower()
+        string=string.replace(" ","")       
+        return string==string[::-1]
+obj=Solution()
+print(obj.isPalindrome("A man, a plan, a canal: Panama"))
+print(obj.isPalindrome("race a car"))
+print(obj.isPalindrome("0P"))
         
