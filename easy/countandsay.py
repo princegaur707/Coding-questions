69545259277720593afbcc3a72b6a7d8cdf2a4ba
proxy707 <princegaur1998@gmail.com>
2020-04-27 23:52:20 +0530

    file changed

--- easy/countandsay.py
+++ easy/countandsay.py
@@ -1,5 +1,7 @@
-def countAndSay(self, n):
+import re
+def countAndSay(n):
     s = '1'
     for _ in range(n - 1):
         s = re.sub(r'(.)\1*', lambda m: str(len(m.group(0))) + m.group(1), s)
     return s
+print(countAndSay(4))
