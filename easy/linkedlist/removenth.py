2f2ce2e3bdf3eaebe8e391feaf65eeccf665ee00
proxy707 <princegaur1998@gmail.com>
2020-04-28 20:55:41 +0530

    file added

---
+++ easy/linkedlist/removenth.py
@@ -0,0 +1,9 @@
+class node:
+    def __init__(data):
+        self.data=data
+        self.next=None
+class Linkedlist:
+    def __init__():
+        self.start=None
+        
+        
--- easy/longestprefix.py
+++ easy/longestprefix.py
@@ -1,7 +1,9 @@
 class Solution:
     def longestCommonPrefix(self, strs):
         rez = ''
+        print("zip(strs):",list(zip(*strs)))
         for letter in zip(*strs):
+            print(letter)
             if len(set(letter)) == 1:
                 rez += letter[0]
             else:
