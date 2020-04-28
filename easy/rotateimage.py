671a01add091df3369d17d5bca0fd5b878a1ebb5
proxy707 <princegaur1998@gmail.com>
2020-04-28 21:10:41 +0530

    file changed

--- easy/rotateimage.py
+++ easy/rotateimage.py
@@ -1,8 +1,8 @@
 class Solution:
     def rotate(self, matrix):
         matrix[::] = zip(*matrix[::-1])#for clockwise rotation
-        matrix1[::] = reversed(zip(*matrix))
-        print("After anti-clockwise:  ",matrix1)
+        #matrix1[::] = reversed(zip(*matrix))
+        #print("After anti-clockwise:  ",matrix1)
         return matrix
 obj=Solution()
 print("After clockwise rotation: ",obj.rotate([[1,2,3],[4,5,6],[7,8,9]]))
