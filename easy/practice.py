b56677b6ce5e3909deaefb85b06a38d8523c73ad
proxy707 <princegaur1998@gmail.com>
2020-04-29 17:37:25 +0530

    file changed

--- easy/practice.py
+++ easy/practice.py
@@ -1,7 +1,8 @@
-class Node():
-    def __init__(self,data):
-        self.data=data
-        self.next=None
-class Linkedlist():
-    def __init__(self):
-        self.
+class Solution:
+    def rotate(self, matrix):
+        matrix1 = zip(*matrix[::-1])#for clockwise rotation
+        matrix2 = reversed(matrix)
+        print("After anti-clockwise:  ",list(matrix2))
+        return list(matrix1)
+obj=Solution()
+print("After clockwise rotation: ",obj.rotate([[1,2,3],[4,5,6],[7,8,9]]))
--- easy/rotateimage.py
+++ easy/rotateimage.py
@@ -1,9 +1,9 @@
 class Solution:
     def rotate(self, matrix):
-        matrix1 = zip(*matrix[::-1])#for clockwise rotation
-        matrix2 = reversed(matrix)
-        print("After anti-clockwise:  ",list(matrix2))
-        return list(matrix1)
+        matrix[::] = zip(*matrix[::-1])#for clockwise rotation
+        #matrix2 = reversed(matrix)
+        #print("After anti-clockwise:  ",list(matrix2))
+        return matrix
 obj=Solution()
 print("After clockwise rotation: ",obj.rotate([[1,2,3],[4,5,6],[7,8,9]]))
 
