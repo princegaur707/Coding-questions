f8dc2525be08bea5d1d458c78090d49cbcc4dda8
proxy707 <princegaur1998@gmail.com>
2020-05-09 21:28:36 +0530

    file changed

--- easy/missingnumbers.py
+++ easy/missingnumbers.py
@@ -4,11 +4,11 @@ class Solution(object):
         # we mark the number that i points as negative.
         # Then we filter the list, get all the indexes
         # who points to a positive number
-        for i in xrange(len(nums)):
+        for i in range(len(nums)):
             index = abs(nums[i]) - 1
             nums[index] = - abs(nums[index])
             print(nums)
         return [i + 1 for i in range(len(nums)) if nums[i] > 0]
 
 obj=Solution()
-obj.findDisappearedNumbers([4,3,2,7,8,2,3,1])
+print(obj.findDisappearedNumbers([4,3,2,7,8,2,3,1]))
