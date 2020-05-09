9a18b7aee0504eea3219f957e2f5689cbccacc28
proxy707 <princegaur1998@gmail.com>
2020-05-10 00:42:57 +0530

    file changed

--- easy/missingnumbers.py
+++ easy/missingnumbers.py
@@ -1,14 +1,13 @@
-class Solution(object):
-    def findDisappearedNumbers(self, nums):
-        # For each number i in nums,
-        # we mark the number that i points as negative.
-        # Then we filter the list, get all the indexes
-        # who points to a positive number
-        for i in range(len(nums)):
-            index = abs(nums[i]) - 1
-            nums[index] = - abs(nums[index])
-            print(nums)
-        return [i + 1 for i in range(len(nums)) if nums[i] > 0]
-
-obj=Solution()
-print(obj.findDisappearedNumbers([4,3,2,7,8,2,3,1]))
+#https://leetcode.com/problems/find-all-numbers-disappeared-in-an-array/
+# For each number i in nums,
+# we mark the number that i points as negative.
+# Then we filter the list, get all the indexes
+# who points to a positive number
+def findDisappearedNumbers(nums):
+    for num in nums:
+        index = abs(num) - 1
+        nums[index] = -abs(nums[index])
+        print("num,index,nums[index]",num,index,nums[index])
+        print("nums:",nums)
+    return [i + 1 for i, num in enumerate(nums) if num > 0]
+print(findDisappearedNumbers([4,3,2,7,8,2,3,1]))
