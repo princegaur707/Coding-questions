dacb57a1eedee1c97c8bd4e9beb6a69fa8b85604
proxy707 <princegaur1998@gmail.com>
2020-05-11 22:21:39 +0530

    files changed

--- easy/findduplicates.py
+++ easy/findduplicates.py
@@ -1,6 +1,17 @@
 #https://leetcode.com/problems/find-all-duplicates-in-an-array/
 class Solution:
     def findDuplicates(self, nums: List[int]) -> List[int]:
+        res=[]
+        for x in nums:
+            if nums[abs(x)-1]<0:
+                res.append(abs(x))
+            else:
+                nums[abs(x)-1]*=-1
+        return res
+                
+        
+class Solution1:
+    def findDuplicates(self, nums: List[int]) -> List[int]:
         arr=[]
         nums=Counter(nums)
         for key,value in nums.items():
--- easy/findjudge.py
+++ easy/findjudge.py
@@ -1,20 +1,15 @@
 class Solution:
-    def findJudge(self, N, trust):
-        arr=arr1=arr2=set()
-        for num in trust:
-            print(num)
-            arr.add(num[0])
-            arr1.add(num[1])
-        print(arr)
-        print(arr1)
-        for i in range(1,N):
-            if i not in arr:
-                for key,value in trust:
-                    if value==i:
-                        arr2.add(key)
-                if(len(arr2)==N-1):
-                    return i
-                else:
-                    return -1
+    def findJudge(self, N: int, trust: List[List[int]]) -> int:
+        trusted = [0] * (N+1)
+        for a, b in trust:
+            trusted[a] -= 1
+            trusted[b] += 1
+
+        for i in range(1, N+1):
+            if trusted[i] == N-1:
+                return i
+        return -1
+                        
+        
 obj=Solution()
 print(obj.findJudge(3,[[1,2],[2,3]]))
