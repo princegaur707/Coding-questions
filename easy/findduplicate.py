ee2150b96989bc01ac6ff9d778411b03f6bc38a5
proxy707 <princegaur1998@gmail.com>
2020-05-12 11:45:08 +0530

    files changed

--- easy/findduplicate.py
+++ easy/findduplicate.py
@@ -9,4 +9,25 @@ class Solution:
         for key,value in nums.items():
             if value>=2:
                 return key
-        
+
+class Solution:
+    
+    def findDuplicate(self, nums):
+        slow = fast = nums[0]
+
+        while True:
+            slow = nums[slow]
+            fast = nums[nums[fast]]
+            print("slow,fast:",slow,fast)
+            if slow == fast:
+                break
+
+        slow = nums[0]
+        while slow != fast:
+            slow = nums[slow]
+            fast = nums[fast]
+
+        return slow
+
+obj=Solution()
+print(obj.findDuplicate([3,1,3,4,2]))
