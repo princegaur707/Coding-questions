f6be7fff857b093333f6cbcffa8a64b490dcc948
proxy707 <princegaur1998@gmail.com>
2020-05-12 12:36:44 +0530

    files changed

--- easy/findjudge.py
+++ easy/findjudge.py
@@ -1,15 +1,14 @@
 class Solution:
-    def findJudge(self, N: int, trust: List[List[int]]) -> int:
+    def findJudge(self, N: int, trust):
         trusted = [0] * (N+1)
         for a, b in trust:
             trusted[a] -= 1
             trusted[b] += 1
-
+            print(trusted)
         for i in range(1, N+1):
             if trusted[i] == N-1:
                 return i
         return -1
-                        
-        
 obj=Solution()
 print(obj.findJudge(3,[[1,2],[2,3]]))
+print(obj.findJudge(3,[[1,3],[2,3]]))
