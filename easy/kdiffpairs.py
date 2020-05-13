19d677823567c0c507cde941b908577c29f4ad50
proxy707 <princegaur1998@gmail.com>
2020-05-13 22:50:21 +0530

    file added

---
+++ easy/kdiffpairs.py
@@ -0,0 +1,7 @@
+def findPairs(self, nums, k):
+        res = 0
+        c = collections.Counter(nums)
+        for i in c:
+            if k > 0 and i + k in c or k == 0 and c[i] > 1:
+                res += 1
+        return res
