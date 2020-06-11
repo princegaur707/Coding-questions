450dc7dc21e64e33e516755996ae1d3323577577
proxy707 <princegaur1998@gmail.com>
2020-06-11 10:04:14 +0530

    files changed

--- BASIC/itertools/iter2.py
+++ BASIC/itertools/iter2.py
@@ -4,14 +4,14 @@ from itertools import combinations,combinations_with_replacement,permutations
 print(list(combinations(range(3),2)))
 print(list(permutations(range(3),2)))
 #OUT:[(0, 1), (0, 2), (1, 2)]  range(3): 0,1,2 elements to be used ---- 2: with 2 elements in one 
-#this is different from permutation as this is also giving [(0,1)(1,0)]
+#this is different from permutation as this removes one of the  [(0,1)(1,0)] unlike permutation
 print("______________________________________________________________________________________________________________________________________________")
 print(list(combinations('abc',2)))
 print("______________________________________________________________________________________________________________________________________________")
 print(list(combinations('abc',3)))
 print("______________________________________________________________________________________________________________________________________________")
-print(list(combinations_with_replacement(range(3),2)))#Repetition is allowed 
+print(list(combinations_with_replacement(range(3),2))) #Repetition is allowed 
 print("______________________________________________________________________________________________________________________________________________")
 print(list(combinations_with_replacement('abc',2)))
 print("______________________________________________________________________________________________________________________________________________")
-print(list(combinations_with_replacement('abc',2))) # it will allow ('a','a') also
+print(list(combinations_with_replacement('abc',2))) #it will allow ('a','a') also
